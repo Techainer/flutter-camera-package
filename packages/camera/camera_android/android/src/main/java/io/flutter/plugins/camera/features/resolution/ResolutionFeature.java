7df8168d46e97cfec022a09960b09a8d9d9bf943
sonuos1501 <118292598+sonuos1501@users.noreply.github.com>
2025-08-04 10:35:18 +0700

    changed resolutions

--- packages/camera/camera_android/android/src/main/java/io/flutter/plugins/camera/features/resolution/ResolutionFeature.java
+++ packages/camera/camera_android/android/src/main/java/io/flutter/plugins/camera/features/resolution/ResolutionFeature.java
@@ -172,16 +172,19 @@ public class ResolutionFeature extends CameraFeature<ResolutionPreset> {
         if (CamcorderProfile.hasProfile(cameraId, CamcorderProfile.QUALITY_HIGH)) {
           return CamcorderProfile.get(cameraId, CamcorderProfile.QUALITY_HIGH);
         }
+        return CamcorderProfile.get(cameraId, CamcorderProfile.QUALITY_720P);
         // fall through
       case ultraHigh:
         if (CamcorderProfile.hasProfile(cameraId, CamcorderProfile.QUALITY_2160P)) {
           return CamcorderProfile.get(cameraId, CamcorderProfile.QUALITY_2160P);
         }
+        return CamcorderProfile.get(cameraId, CamcorderProfile.QUALITY_720P);
         // fall through
       case veryHigh:
         if (CamcorderProfile.hasProfile(cameraId, CamcorderProfile.QUALITY_1080P)) {
           return CamcorderProfile.get(cameraId, CamcorderProfile.QUALITY_1080P);
         }
+        return CamcorderProfile.get(cameraId, CamcorderProfile.QUALITY_720P);
         // fall through
       case high:
         if (CamcorderProfile.hasProfile(cameraId, CamcorderProfile.QUALITY_720P)) {
@@ -226,16 +229,19 @@ public class ResolutionFeature extends CameraFeature<ResolutionPreset> {
         if (CamcorderProfile.hasProfile(cameraId, CamcorderProfile.QUALITY_HIGH)) {
           return CamcorderProfile.getAll(cameraIdString, CamcorderProfile.QUALITY_HIGH);
         }
+        return CamcorderProfile.getAll(cameraIdString, CamcorderProfile.QUALITY_720P);
         // fall through
       case ultraHigh:
         if (CamcorderProfile.hasProfile(cameraId, CamcorderProfile.QUALITY_2160P)) {
           return CamcorderProfile.getAll(cameraIdString, CamcorderProfile.QUALITY_2160P);
         }
+        return CamcorderProfile.getAll(cameraIdString, CamcorderProfile.QUALITY_720P);
         // fall through
       case veryHigh:
         if (CamcorderProfile.hasProfile(cameraId, CamcorderProfile.QUALITY_1080P)) {
           return CamcorderProfile.getAll(cameraIdString, CamcorderProfile.QUALITY_1080P);
         }
+        return CamcorderProfile.getAll(cameraIdString, CamcorderProfile.QUALITY_720P);
         // fall through
       case high:
         if (CamcorderProfile.hasProfile(cameraId, CamcorderProfile.QUALITY_720P)) {
